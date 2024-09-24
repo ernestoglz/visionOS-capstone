442d6f7e7e8c90e2c97e98392a06fa3cd972aa0a
Ernesto Gonzalez <egonza2692@gmail.com>
2024-09-23 18:39:32 -0600

    Initial Commit

---
+++ Capstone/CapstoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapstoneApp.swift
+//  Capstone
+//
+//  Created by Ernesto González on 23/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CapstoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
